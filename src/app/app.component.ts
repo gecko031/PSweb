7c2181188f0697bd103887207dd0494e023be91d
gecko031 <pawel.slepaczuk@pollub.edu.pl>
2018-10-31 15:29:56 +0100

    app working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,59 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'PSweb';
 
-  group = 'wolf';
-  preyType = 'sheep';
+  newTask: string;
+  tasksList: Array<string> = [];
+  tasksDone: Array<string> = [];
 
-  /*  event binding
-  inputText = 'text';
-  colorClass = 'color';
-  isDisable = true;
-  */
-  /*  property binding
-  logoUrl = '';
-  maxLength = 5;
-  */
-
-  constructor() {
-/* property binding
-   setTimeout(() => {
-      this.isDisable = false;
-    }, 3000);*/
+  add() {
+    this.tasksList.push(this.newTask);
+    this.newTask = '';
   }
-/* property + event binding
-  inputG(event) {
-    this.group = event.target.value;
+  remove(task: string) {
+    this.tasksList = this.tasksList.filter( e => e !== task);
   }
-  inputPT(event) {
-    this.preyType = event.target.value;
-  }*/
+  markDone(task: string) {
+    this.tasksDone.push(task);
+    this.remove(task);
+  }
 
-
-  /*  event binding
-  onFocus() {
-    this.colorClass = 'color2';
-  }
-  onClick(event) {
-    console.log(event);
-  }
-  onMouseMovement(event) {
-    console.log('x: ' + event.clientX + ', y: ' + event.clientY);
-  }
-  onPaste(event) {
-    this.inputText = 'just...DONT';
-  }
-  change() {
-    this.isDisable = false;
-  }*/
-
-  /*property binding
-  change() {
-    this.inputText = 'Text and color change';
-    this.maxLength = 10;
-    this.colorClass = 'color2';
-    this.logoUrl = 'http://vignette4.wikia.nocookie.net/lumber-tycoon-2/images/5/57/Pepe.png/revision/latest';
-
-  }*/
 }
