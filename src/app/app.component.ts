a2035a5c10b4e5db3d7ec830c0a79c967c0cf1a8
gecko031 <pawel.slepaczuk@pollub.edu.pl>
2018-10-31 10:22:08 +0100

    event binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,26 +6,47 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-
   title = 'PSweb';
-  inputText = 'text';
-  maxLength = 5;
-  colorClass = 'color';
-  logoUrl = '';
 
+  inputText = 'text';
+  colorClass = 'color';
   isDisable = true;
+  /*
+  *property binding
+  logoUrl = '';
+  maxLength = 5;
+  */
 
 
   constructor() {
-    setTimeout(() => {
+/* property binding
+   setTimeout(() => {
       this.isDisable = false;
-    }, 3000);
+    }, 3000);*/
   }
 
+  onFocus() {
+    this.colorClass = 'color2';
+  }
+  onClick(event) {
+    console.log(event);
+  }
+  onMouseMovement(event) {
+    console.log('x: ' + event.clientX + ', y: ' + event.clientY);
+  }
+  onPaste(event) {
+    this.inputText = 'just...DONT';
+  }
+  change() {
+    this.isDisable = false;
+  }
+
+  /*property binding
   change() {
     this.inputText = 'Text and color change';
     this.maxLength = 10;
     this.colorClass = 'color2';
     this.logoUrl = 'http://vignette4.wikia.nocookie.net/lumber-tycoon-2/images/5/57/Pepe.png/revision/latest';
-  }
+
+  }*/
 }
