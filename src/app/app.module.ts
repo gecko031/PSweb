4d2c5ebaecea493e5323b66f290278c87c056634
gecko031 <pawel.slepaczuk@pollub.edu.pl>
2018-11-04 11:45:38 +0100

    input

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { FirstComponent } from './first/first.component';
-import { SecondComponent } from './second/second.component';
-import { TempComponent } from './temp/temp.component';
 import {FormsModule} from '@angular/forms';
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    FirstComponent,
-    SecondComponent,
-    TempComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
