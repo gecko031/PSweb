4167f388a1a82bedcae07e36a9699dbc9fac2223
gecko031 <pawel.slepaczuk@pollub.edu.pl>
2019-03-11 13:16:20 +0100

    display through ng-content tag

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import {ChildComponent} from './child/child.component';
+import { ChildContentComponent } from './child-content/child-content.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ChildComponent
+    ChildComponent,
+    ChildContentComponent
   ],
   imports: [
     BrowserModule
