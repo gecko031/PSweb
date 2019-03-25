9e9fdba4abb1980dd08f590297370cefadd78f59
gecko031 <pawel.slepaczuk@pollub.edu.pl>
2019-03-25 22:34:45 +0100

    todo app prepared for database implementation

--- src/app/services/tasks.services.ts
+++ src/app/services/tasks.services.ts
@@ -4,28 +4,28 @@ import { Task } from '../model/task.model';
 
 @Injectable()
 export class TasksService {
-    private tasksList: Array<Task> = [];
 
     private tasksListObservable = new BehaviorSubject<Array<Task>>([]);
 
     constructor() {
-        this.tasksList = [
+        const tasksList = [
             {name: 'Homework', created: new Date().toLocaleString(), isDone: false},
             {name: 'wash dishes', created: new Date().toLocaleString(), isDone: false},
             {name: 'have a beer', created: new Date().toLocaleString(), isDone: false},
             {name: 'Dont have a beer', created: new Date().toLocaleString(), end: new Date().toLocaleString(),  isDone: true},
             {name: 'have a snack', created: new Date().toLocaleString(), isDone: false}];
-        this.tasksListObservable.next(this.tasksList);
+        this.tasksListObservable.next(tasksList);
     }
 
     add(task: Task) {
-        this.tasksList.push(task);
-        this.tasksListObservable.next(this.tasksList);
+        const list = this.tasksListObservable.getValue();
+        list.push(task);        
+        this.tasksListObservable.next(list);
     }
 
     remove(task: Task) {
-        this.tasksList = this.tasksList.filter( e => e !== task);
-        this.tasksListObservable.next(this.tasksList);
+        const list = this.tasksListObservable.getValue().filter( e => e !== task);
+        this.tasksListObservable.next(list);
     }
 
     markDone(task: Task) {
